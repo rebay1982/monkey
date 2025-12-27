e1a2028aea68393044ad94917c2b3ea172c20b1a
Reda Baydoun <reda.baydoun@gmail.com>
2025-12-27 15:12:32 -0500

    feat: add return statement handling

--- ast/ast.go
+++ ast/ast.go
@@ -48,3 +48,12 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+// RETURN
+type ReturnStatement struct {
+	Token       token.Token // the token.RETURN token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
