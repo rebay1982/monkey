3d0da9befcab44fb04a6ff3d75604a025a233348
Reda Baydoun <reda.baydoun@gmail.com>
2026-02-15 12:26:47 -0500

    feat: handle functions, calls, and closures

--- object/object.go
+++ object/object.go
@@ -1,17 +1,22 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+
+	"monkey/ast"
 )
 
 type ObjectType string
 
 const (
-	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
+	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUCNTION"
+	INTEGER_OBJ      = "INTEGER"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ        = "ERROR"
 )
 
 type Object interface {
@@ -56,3 +61,29 @@ type Error struct {
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+// FUNCTION object
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
