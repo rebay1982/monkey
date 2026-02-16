02ec3ceb292069ab7d17e89ae47f6787935fe7b4
Reda Baydoun <reda.baydoun@gmail.com>
2026-02-16 11:42:35 -0500

    feat: add arrays and indexing

--- ast/ast.go
+++ ast/ast.go
@@ -273,7 +273,7 @@ func (ce *CallExpression) String() string {
 	return out.String()
 }
 
-// STRING
+// STRINGS
 type StringLiteral struct {
 	Token token.Token
 	Value string
@@ -282,3 +282,47 @@ type StringLiteral struct {
 func (sl *StringLiteral) expressionNode()      {}
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
 func (sl *StringLiteral) String() string       { return sl.Token.Literal }
+
+// ARRAYS
+type ArrayLiteral struct {
+	Token    token.Token // the '[' token
+	Elements []Expression
+}
+
+func (al *ArrayLiteral) expressionNode()      {}
+func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
+
+// ARRAY INDEX EXPRESSION
+type IndexExpression struct {
+	Token token.Token // The [ token
+	Left  Expression  // Object being "indexed"
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
