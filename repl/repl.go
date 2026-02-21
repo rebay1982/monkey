d31ac9a4f50e53455d0ef5ff41d60f719080979a
Reda Baydoun <reda.baydoun@gmail.com>
2026-02-20 21:46:18 -0500

    feat: fix repl bug

--- repl/repl.go
+++ repl/repl.go
@@ -51,6 +51,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -68,8 +69,6 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		env := object.NewEnvironment()
 
 		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
